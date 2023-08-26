120fbae9cb36c177f56a8202d5d2063eda10af64
ChoiWheatley <chltmdgus604@naver.com>
2023-08-26 13:15:24 +0000

    [ADD - SHC] 9251 LCS 메모리 초과

---
+++ 07_Q9251/Q9251_SHC.py
@@ -0,0 +1,28 @@
+"""
+LCS
+"""
+
+from functools import cache
+from sys import setrecursionlimit
+
+setrecursionlimit(10**8)
+
+
+@cache
+def LCS(X: str, Y: str) -> int:
+    """X와 Y의 최장 공통부분수열의 길이를 구한다."""
+    if len(X) == 0 or len(Y) == 0:
+        return 0
+
+    if X[-1] == Y[-1]:
+        # 두 접두어가 같은 문자로 끝난다 => LCS의 멤버임이 확실
+        return LCS(X[:-1], Y[:-1]) + 1
+    # 두 접두어가 다른 문자로 끝난다. xn을 살리는 쪽과 yn을 살리는 쪽
+    # 둘 중에 더 긴 녀석을 선택한다.
+    return max(LCS(X[:-1], Y), LCS(X, Y[:-1]))
+
+
+if __name__ == "__main__":
+    X = input()
+    Y = input()
+    print(LCS(X, Y))
---
+++ 07_Q9251/README.md
@@ -0,0 +1,18 @@
+# 07_Q9251 LCS
+
+최장공통부분수열의 길이를 구하는 문제.
+
+부분수열의 정의: 수열 S에서 순서를 바꾸지 않고 임의의 원소를 꺼낸 모든 수열을 S의 부분수열이라고 부른다.
+
+예: 
+
+```python
+S = "Hello, World!"
+possible_subsequence = [
+    "Hello",
+    "World",
+    "Hold",
+    "ello",
+    ...
+]
+```
