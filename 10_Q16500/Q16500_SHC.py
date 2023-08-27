987ca1179e9c0070e69d83c3e06970d11ca05532
ChoiWheatley <chltmdgus604@naver.com>
2023-08-27 06:18:00 +0000

    [UPDATE - SHC] 16500 문자열 판별 ACCEPT

--- 10_Q16500/Q16500_SHC.py
+++ 10_Q16500/Q16500_SHC.py
@@ -1,8 +1,8 @@
 """
 문자열 판별
 
-메모리:
-실행시간:
+메모리 KB: 38892
+실행시간 ms: 152
 """
 
 from sys import stdin
@@ -20,29 +20,22 @@ def djbc2(s: str) -> int:
 
 
 def sol(S: str, A: Sequence[str]) -> bool:
-    # left to right
-    start = 0
-    end = 1
+    """
+    S 문자열을 A의 원소들을 공백없이, 중복을 허용하여 만들 수 있는지 구하시오.
+    """
     A_HASH = {djbc2(a) for a in A}
 
-    while end <= len(S):
-        sample = S[start:end]
-        if djbc2(sample) in A_HASH:
-            start = end
-        end += 1
-    if start == end - 1:
-        return True
+    # dp[i] = S[i:end]이 A 안에 존재하는가? where end = 마지막으로 찾은 단어의 첫 인덱스 또는 문자열 맨 끝
+    # 쉽게 말해 True인 곳들을 다 탐방하는겨
+    dp = [False for _ in range(len(S) + 1)]
+    dp[len(S)] = True
 
-    # right to left
-    rstart = len(S)
-    rend = len(S) - 1
-    while rend >= 0:
-        sample = S[rend:rstart]
-        if djbc2(sample) in A_HASH:
-            rstart = rend
-        rend -= 1
+    for start in range(len(S) - 1, -1, -1):
+        for end in range(start + 1, len(S) + 1):
+            if dp[end] == 1 and djbc2(S[start:end]) in A_HASH:
+                dp[start] = True
 
-    return rstart == rend + 1
+    return dp[0]
 
 
 if __name__ == "__main__":
