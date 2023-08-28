2e773da4d71d0c940bf2da664516ff120f07d5f8
BellPumpkin <whdgh135@gmail.com>
2023-08-28 22:07:09 +0900

    [ Update - JHP ] Q11053_가장 긴 증가하는 부분 수열_accept

--- 09_Q11053/Q11053_JHP.py
+++ 09_Q11053/Q11053_JHP.py
@@ -1,1 +1,22 @@
-drtyfytdgfytdfytryuy
+'''
+    [효율성]
+    - 메모리: 31256KB	
+    - 시간: 152ms
+'''
+n = int(input())
+
+ls = [0]
+ls.extend(list(map(int, input().split())))
+
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    maxi = 0
+    for j in range(0, i):
+        # ls[i] 보다 작은 값
+        if ls[i] > ls[j]:
+            # 중에서 가장 큰 값
+            maxi = max(maxi, dp[j])
+    dp[i] = maxi + 1
+
+print(max(dp))
