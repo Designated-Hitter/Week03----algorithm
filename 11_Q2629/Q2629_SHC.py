2e6314aab2d4fe23c6c4b6f7622eb2cfe6eb9922
ChoiWheatley <chltmdgus604@naver.com>
2023-08-28 01:34:10 +0000

    [UPDATE - SHC] 2629 양팔저울 Index Error

--- 11_Q2629/Q2629_SHC.py
+++ 11_Q2629/Q2629_SHC.py
@@ -2,30 +2,48 @@
 양팔저울
 """
 
-from collections import defaultdict
-from functools import cache
 from sys import stdin
 
+MAX_WEIGHT = 40
+MAX_N = 7
 
-@cache
-def r_sol(weight: int, visited: list[int]) -> bool:
-    """weight 무게를 추들의 모임 W를 사용해서 만들 수 있는가?"""
-    global W
-    if 
+
+def r_sol(idx: int, weight: int):
+    if g_visited[idx][weight]:
+        return
+
+    g_visited[idx][weight] = True
+
+    if idx >= len(W):
+        return
+
+    r_sol(idx + 1, weight + W[idx])
+    r_sol(idx + 1, weight)  # pass
+    r_sol(idx + 1, abs(weight - W[idx]))
 
 
 # key: weight, value: quantity
-W: dict[int, int] = defaultdict(int)
+W: list[int]
+# visited[idx][weight]: idx번째 추 까지 사용하여 weight를 만들 수 있는가?
+g_visited: list[list[bool]]
 
 
 if __name__ == "__main__":
     n = int(stdin.readline().strip())
-    for weight in (int(x) for x in stdin.readline().split()):
-        W[weight] += 1
-
-    print(W)
+    W = sorted(int(x) for x in stdin.readline().split())
 
     k = int(stdin.readline().strip())
     marbles = [int(x) for x in stdin.readline().split()]
 
-    print(" ".join("Y" if r_sol(marble) else "N" for marble in marbles))
+    g_visited = [[False for _ in range(MAX_WEIGHT + 1)] for _ in range(n + 1)]
+
+    r_sol(0, 0)
+
+    for marble in marbles:
+        if marble > MAX_WEIGHT:
+            print("N", end=" ")
+        elif g_visited[n][marble]:
+            print("Y", end=" ")
+        else:
+            print("N", end=" ")
+    print()
