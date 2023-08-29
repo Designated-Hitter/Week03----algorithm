e1890ab284464a4c61334310016800459491a6ee
Designated-Hitter <dosadola@gmail.com>
2023-08-29 11:53:41 +0900

    [ADD - DHJ]Q2253 solved

--- 19_Q2253/Q2253_DHJ.py
+++ 19_Q2253/Q2253_DHJ.py
@@ -12,6 +12,19 @@ for _ in range(M):
     small_stones.append(small_stone)
 
 print(small_stones)
-dp = [[sys.maxsize] * N for _ in range(N)]
+dp = [[float('inf')] * (int((2*N)**0.5) + 2) for _ in range(N + 1)]
+#(2*N) ** 0.5) + 1은 첫째항이 1이고 공차가 1인 등차수열에서 수열의 합이 N이 될 때 마지막 항의 근사값, 즉 돌의 갯수가 N개일 때 나올 수 있는 속도의 최대값에 가까운 값
+dp[1][0] = 0
 
 print(dp)
+
+for i in range(2, N + 1):
+    if i in small_stones:
+        continue
+    for j in range(1, int((2*i)**0.5) + 1):
+        dp[i][j] = min(dp[i-j][j-1], dp[i-j][j], dp[i-j][j+1]) + 1
+#점화식: i라는 점에 j라는 속도로 도달했을 때 최소 점프 횟수는 i-j의 위치에 따라 (각각 j-1, j, j+1의 속도로 도달했을 때 시행한 점프) + 1
+if min(dp[N]) == float('inf'): #목표지점까지 갈 수 없음
+    print(-1)
+else:
+    print(min(dp[N])) #dp[N]은 각각의 속도로 N에 도착했을 때 얻을 수 있는 점프의 여러 값들이 배열로 구성되어 있는데, 그 중 최소값을 출력
